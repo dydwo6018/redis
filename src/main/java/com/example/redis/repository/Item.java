4ee477473d1fbd1d541b6b8d7e08d90c91d3d2ef
dydwo6018 <dydwo6018@gmail.com>
2025-03-06 14:34:16 +0900

    Redis활용한 Item & Test코드

---
+++ src/main/java/com/example/redis/repository/Item.java
@@ -0,0 +1,24 @@
+package com.example.redis.repository;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+// Entity 대신 RedisHash
+@RedisHash("item")
+public class Item implements Serializable {
+    @Id
+    // ID String 으로 쓰면 UUID가 자동으로 배정된다.
+    private String id;
+    private String name;
+    private String description;
+    private Integer price;
+
+}
---
+++ src/main/java/com/example/redis/repository/ItemRepository.java
@@ -0,0 +1,6 @@
+package com.example.redis.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ItemRepository extends CrudRepository<Item, String > {
+}

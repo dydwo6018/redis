6c628d336d2b40ba84f417306efa46595fe4f583
dydwo6018 <dydwo6018@gmail.com>
2025-03-06 15:55:35 +0900

    Http Session 과 Session Clustering

--- src/main/java/com/example/redis/RedisConfig.java
+++ src/main/java/com/example/redis/RedisConfig.java
@@ -19,4 +19,9 @@ public class RedisConfig {
         template.setValueSerializer(RedisSerializer.json());
         return template;
     }
+
+    @Bean
+    public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
+        return RedisSerializer.java();
+    }
 }
